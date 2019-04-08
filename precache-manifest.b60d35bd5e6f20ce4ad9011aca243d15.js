e888e35892c07d5836df6f4d466316ad2aa7827b
Pankaj Kumar Singh <pankaj28843@gmail.com>
2019-04-08 11:51:34 +0200

    published new build

---
+++ precache-manifest.b60d35bd5e6f20ce4ad9011aca243d15.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/sample-vue-project/robots.txt"
+  },
+  {
+    "revision": "d46894c7453b30361020",
+    "url": "/sample-vue-project/js/chunk-vendors.588b6f36.js"
+  },
+  {
+    "revision": "4cf60835f98272d19e39",
+    "url": "/sample-vue-project/js/app.3cfdc986.js"
+  },
+  {
+    "revision": "d7c849ad540c6c2a0455",
+    "url": "/sample-vue-project/js/about.ad763791.js"
+  },
+  {
+    "revision": "0539b308d1397097eb88f5f9d9e5936e",
+    "url": "/sample-vue-project/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/sample-vue-project/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "4cf60835f98272d19e39",
+    "url": "/sample-vue-project/css/app.68fa37a6.css"
+  }
+];
